3a5c5cb01f4f138fee93768c1b92d47d975d8078
michaeloponteski <michal.pokladowski@gmail.com>
2025-05-11 15:36:36 +0200

    Minifier logic

--- src/main/java/pl/put/poznan/processor/rest/RESTController.java
+++ src/main/java/pl/put/poznan/processor/rest/RESTController.java
@@ -22,37 +22,4 @@ public class RESTController {
         return processor.processJSON(json);
     }
 
-    // Prettify JSON
-//    @PostMapping(value = "/prettify", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public String prettify(@RequestBody String json) {
-//        logger.debug("Prettifying JSON: {}", json);
-//        JSONProcessor processor = new PrettifyDecorator(new BasicJSONProcessor());
-//        return processor.processJSON(json);
-//    }
-//
-//    // Filter JSON by keys
-//    @PostMapping(value = "/filter", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public String filter(@RequestBody String json, @RequestParam List<String> keys) {
-//        logger.debug("Filtering JSON: {} with keys: {}", json, keys);
-//        KeyFilterDecorator processor = new KeyFilterDecorator(new BasicJSONProcessor());
-//        processor.setKeysToInclude(keys);
-//        return processor.processJSON(json);
-//    }
-//
-//    // Exclude keys from JSON
-//    @PostMapping(value = "/exclude", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public String exclude(@RequestBody String json, @RequestParam List<String> keys) {
-//        logger.debug("Excluding keys: {} from JSON: {}", keys, json);
-//        KeyExcludeDecorator processor = new KeyExcludeDecorator(new BasicJSONProcessor());
-//        processor.setKeysToExclude(keys);
-//        return processor.processJSON(json);
-//    }
-//
-//    // Compare two JSON texts
-//    @PostMapping(value = "/compare", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public String compare(@RequestParam String text1, @RequestParam String text2) {
-//        logger.debug("Comparing JSON texts.");
-//        TextCompareDecorator processor = new TextCompareDecorator(new BasicJSONProcessor());
-//        return processor.compareTexts(text1, text2);
-//    }
 }
