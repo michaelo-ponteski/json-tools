931fffcc6ddacd91043ed36e92e7c3943ef1f119
Tomasz <tomekpres180@gmail.com>
2025-05-11 15:42:53 +0200

    Adding prettify_decorator

--- src/main/java/pl/put/poznan/processor/app/JsonToolsApplication.java
+++ src/main/java/pl/put/poznan/processor/app/JsonToolsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.processor.rest"})
-public class JSONToolsApplication {
+class JSONToolsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JSONToolsApplication.class, args);
